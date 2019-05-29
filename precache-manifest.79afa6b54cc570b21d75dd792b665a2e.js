81890c44126e5f252d2a8f3dd7f7285033d4d133
Camilia <mzalicam@gmail.com>
2019-05-29 13:10:14 -0400

    Updates

---
+++ precache-manifest.79afa6b54cc570b21d75dd792b665a2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa97da10311b5f3fc0e7d65fc38ba5b",
+    "url": "/clicky-memory-game/index.html"
+  },
+  {
+    "revision": "8b8bb132f8d73d52281e",
+    "url": "/clicky-memory-game/static/css/main.1717a4e5.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clicky-memory-game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "8b8bb132f8d73d52281e",
+    "url": "/clicky-memory-game/static/js/main.e0408bdb.chunk.js"
+  },
+  {
+    "revision": "2d83433aa8872a544556",
+    "url": "/clicky-memory-game/static/js/runtime~main.83671481.js"
+  }
+]);
